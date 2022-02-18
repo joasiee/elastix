fc058711783c61461e713a637df4d3351670f29a
Joas Mulder <joasmulder@hotmail.com>
2022-02-18 01:47:09 +0100

    conv test

--- experiments/experiments/convergence_tests.py
+++ experiments/experiments/convergence_tests.py
@@ -10,7 +10,7 @@ from elastix_wrapper.parameters import Collection, Parameters
 instances = [16, 23, 17, 13, 6, 26, 5]
 
 for instance in instances:
-    params = Parameters(mesh_size=8).gomea(fos=-6, partial_evals=True).stopping_criteria(iterations=50).instance(Collection.EMPIRE, instance)
+    params = Parameters(mesh_size=8).gomea(fos=-6, partial_evals=True).stopping_criteria(iterations=30).instance(Collection.EMPIRE, instance)
     run = wandb.init(project="convergence_tests", name=str(params), reinit=True)
     wandb.config.instance = instance
     wrapper.run(params, Path("output") / wandb.run.project / wandb.run.name)
