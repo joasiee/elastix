8d8064ebe68e3e8abec8a0dbb6db44e78f2714ae
Joas Mulder <joasmulder@hotmail.com>
2022-03-20 17:41:24 +0100

    pareto front random sampling

--- experiments/experiments/pareto_front.py
+++ experiments/experiments/pareto_front.py
@@ -20,33 +20,32 @@ iterations_a = [50000, 100000]
 
 GOMEA = bool(int(sys.argv[1]))
 optimizer = "GOMEA" if GOMEA else "AdaptiveStochasticGradientDescent"
-entity, project = "joasiee", "pareto_front"
+entity, project = "joasiee", "pareto_front_sampling"
 
 for instance in instances:
-    for weight0 in np.arange(0.01, 0.11, 0.01):
-        for weight1 in np.arange(0.05, 1.05, 0.05):
-            weight0 = np.around(weight0, 2)
-            weight1 = np.around(weight1, 2)
+    while True:
+        weight0 = np.around(np.random.uniform(0.001, 0.101), 3)
+        weight1 = np.around(np.random.uniform(0.001, 1.001), 2)
 
-            #check if already done
-            filters = {"state": {"$in": ["finished", "running"]}, "config.Metric0Weight": float(weight0), "config.Metric1Weight": float(weight1), "config.Optimizer": optimizer, "config.Instance": instance}
-            api = wandb.Api()
-            runs = api.runs(entity + "/" + project, filters=filters)
-            if len(runs) > 0:
-                logger.info(f"Skipping run with {filters}")
-                continue
+        #check if already done
+        filters = {"state": {"$in": ["finished", "running"]}, "config.Metric0Weight": float(weight0), "config.Metric1Weight": float(weight1), "config.Optimizer": optimizer, "config.Instance": instance}
+        api = wandb.Api()
+        runs = api.runs(entity + "/" + project, filters=filters)
+        if len(runs) > 0:
+            logger.info(f"Skipping run with {filters}")
+            continue
 
-            params = (Parameters.from_base(mesh_size=mesh_size)
-                    .multi_metric(weight0=weight0, weight1=weight1)
-                    .multi_resolution(n=2, p_sched=sched)
-                    .instance(Collection.EMPIRE, instance)
-                    )
-            if GOMEA:
-                params.gomea(fos=-6, partial_evals=True).stopping_criteria(iterations=iterations_g)
-            else:
-                params.optimizer(optimizer).stopping_criteria(iterations=iterations_a)
-            run = wandb.init(project=project,
-                            name=str(params), reinit=True)
-            params.prune()
-            wandb.config.update(params.params)
-            wrapper.run(params, Path("output") / wandb.run.project / wandb.run.name)
+        params = (Parameters.from_base(mesh_size=mesh_size)
+                .multi_metric(weight0=weight0, weight1=weight1)
+                .multi_resolution(n=2, p_sched=sched)
+                .instance(Collection.EMPIRE, instance)
+                )
+        if GOMEA:
+            params.gomea(fos=-6, partial_evals=True).stopping_criteria(iterations=iterations_g)
+        else:
+            params.optimizer(optimizer).stopping_criteria(iterations=iterations_a)
+        run = wandb.init(project=project,
+                        name=str(params), reinit=True)
+        params.prune()
+        wandb.config.update(params.params)
+        wrapper.run(params, Path("output") / wandb.run.project / wandb.run.name)
