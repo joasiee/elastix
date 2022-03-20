e3ed04657fb67ace31229867b7956b535a52b8be
Joas Mulder <joasmulder@hotmail.com>
2022-03-20 01:28:44 +0100

    run experiment this way for now

--- experiments/experiments/pareto_front.py
+++ experiments/experiments/pareto_front.py
@@ -12,15 +12,11 @@ import logging
 
 logger = logging.getLogger("ParetoFront")
 
-
 mesh_size = 8
 instances = [16, 23, 17, 13, 6] if len(sys.argv) == 2 else [int(sys.argv[2])]
-downsampling_f = 4
-sched = [1.5, 1.5, 1.5, 1, 1, 1]
+sched = [5, 5, 5, 3, 3, 3]
 iterations_g = [33, 100]
-iterations_a = [100000, 300000]
-max_time = 7200
-seed = 18926
+iterations_a = [50000, 100000]
 
 GOMEA = bool(int(sys.argv[1]))
 optimizer = "GOMEA" if GOMEA else "AdaptiveStochasticGradientDescent"
@@ -28,10 +24,10 @@ entity, project = "joasiee", "pareto_front"
 
 for instance in instances:
     for weight0 in np.arange(0.01, 0.11, 0.01):
-        for weight1 in np.arange(0.02, 0.42, 0.02):
+        for weight1 in np.arange(0.05, 1.05, 0.05):
             weight0 = np.around(weight0, 2)
             weight1 = np.around(weight1, 2)
-            
+
             #check if already done
             filters = {"state": {"$in": ["finished", "running"]}, "config.Metric0Weight": float(weight0), "config.Metric1Weight": float(weight1), "config.Optimizer": optimizer, "config.Instance": instance}
             api = wandb.Api()
@@ -40,15 +36,15 @@ for instance in instances:
                 logger.info(f"Skipping run with {filters}")
                 continue
 
-            params = (Parameters.from_base(mesh_size=mesh_size, downsampling_f=downsampling_f, seed=seed)
+            params = (Parameters.from_base(mesh_size=mesh_size)
                     .multi_metric(weight0=weight0, weight1=weight1)
                     .multi_resolution(n=2, p_sched=sched)
                     .instance(Collection.EMPIRE, instance)
                     )
             if GOMEA:
-                params.gomea(fos=-6, partial_evals=True).stopping_criteria(max_time_s=max_time, iterations=iterations_g)
+                params.gomea(fos=-6, partial_evals=True).stopping_criteria(iterations=iterations_g)
             else:
-                params.optimizer(optimizer).stopping_criteria(max_time_s=max_time, iterations=iterations_a)
+                params.optimizer(optimizer).stopping_criteria(iterations=iterations_a)
             run = wandb.init(project=project,
                             name=str(params), reinit=True)
             params.prune()
