63b72022712a9f4c3790b0dea4c5a7c3f14548b3
Joas Mulder <joasmulder@hotmail.com>
2022-03-24 13:06:35 +0100

    run queue shark

--- experiments/experiments/generate_experiments.py
+++ experiments/experiments/generate_experiments.py
@@ -1,6 +1,24 @@
 import logging
 
+import numpy as np
+from elastix_wrapper.parameters import Parameters, Collection
+from experiments.experiment import Experiment, ExperimentQueue
+
 logger = logging.getLogger("ParetoFront")
 
+def sampling_p_range(instance: int, project: str):
+    for sampling_p in np.arange(0.01, 0.21, 0.01):
+        params = (
+            Parameters.from_base(mesh_size=2, seed=1523, sampling_p=sampling_p)
+            .multi_resolution(1, p_sched=[7, 7, 7])
+            .multi_metric()
+            .gomea()
+            .instance(Collection.EMPIRE, 26)
+            .stopping_criteria(iterations=[100])
+        )
+        yield Experiment(params, project)
+
 if __name__ == "__main__":
-    pass
+    queue = ExperimentQueue()
+    for experiment in sampling_p_range(26, "gomea_uni_sampling"):
+        queue.push(experiment)
--- experiments/experiments/run_local.py
+++ experiments/experiments/run_local.py
@@ -10,13 +10,13 @@ from elastix_wrapper.parameters import Parameters, Collection
 from experiments.experiment import Experiment, run_experiment
 
 
-for sampling_p in np.arange(0.01, 0.2, 0.01):
-    params = (
-        Parameters.from_base(mesh_size=2, seed=1523, sampling_p=sampling_p)
-        .multi_resolution(1, p_sched=[7, 7, 7])
-        .asgd()
-        .instance(Collection.EMPIRE, 26)
-        .stopping_criteria(iterations=[20000])
-    )
+params = (
+    Parameters.from_base(mesh_size=2, seed=1523, sampling_p=0.2)
+    .multi_resolution(1, p_sched=[7, 7, 7])
+    .multi_metric()
+    .asgd()
+    .instance(Collection.EMPIRE, 26)
+    .stopping_criteria(iterations=[20000])
+)
 
-    run_experiment(Experiment(params))
+run_experiment(Experiment(params, "asgd_26_sampling"))
