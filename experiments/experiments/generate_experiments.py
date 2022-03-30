b6411324b567e59b60f0da5fefd3843974ddb9f9
Joas Mulder <joasmulder@hotmail.com>
2022-03-30 18:37:56 +0200

    sampling experiments

--- experiments/experiments/generate_experiments.py
+++ experiments/experiments/generate_experiments.py
@@ -7,19 +7,19 @@ from experiments.experiment import Experiment, ExperimentQueue
 logger = logging.getLogger("ParetoFront")
 
 def sampling_p_range(instance: int, project: str):
-    for sampling_p in np.arange(0.01, 0.21, 0.01):
+    for sampling_p in [0.01, 0.02, 0.03] + list(np.arange(0.05, 0.55, 0.05)):
         for seed in [2356, 3487, 4942, 1432]:
             params = (
                 Parameters.from_base(mesh_size=2, seed=seed, sampling_p=sampling_p)
                 .multi_resolution(1, p_sched=[7, 7, 7])
                 .multi_metric()
-                .asgd()
+                .gomea(fos=-6, partial_evals=True)
                 .instance(Collection.EMPIRE, 26)
-                .stopping_criteria(iterations=[5000])
+                .stopping_criteria(iterations=[200])
             )
             yield Experiment(params, project)
 
 if __name__ == "__main__":
     queue = ExperimentQueue()
-    for experiment in sampling_p_range(26, "asgd_26_sampling"):
+    for experiment in sampling_p_range(26, "gomea_sampling"):
         queue.push(experiment)
--- experiments/experiments/run_local.py
+++ experiments/experiments/run_local.py
@@ -8,12 +8,12 @@ from elastix_wrapper.parameters import Parameters, Collection
 from experiments.experiment import Experiment, run_experiment
 
 params = (
-    Parameters.from_base(mesh_size=3, seed=1523, sampling_p=0.05)
+    Parameters.from_base(mesh_size=2, seed=1523, sampling_p=0.2)
     .multi_resolution(1, p_sched=[7, 7, 7])
     .multi_metric()
     .gomea(fos=-6, partial_evals=True)
     .instance(Collection.EMPIRE, 26)
-    .stopping_criteria(iterations=[100])
+    .stopping_criteria(iterations=[200])
 )
 
-run_experiment(Experiment(params, "gomea_conv"))
+run_experiment(Experiment(params, "zandbak"))
