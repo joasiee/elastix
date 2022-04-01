9d847761ae4e2dd05bd4ceed8231de87176b3206
Joas Mulder <joasmulder@hotmail.com>
2022-04-01 16:34:12 +0200

    poetry dev dep

--- experiments/experiments/generate_experiments.py
+++ experiments/experiments/generate_experiments.py
@@ -10,16 +10,16 @@ def sampling_p_range(instance: int, project: str):
     for sampling_p in [0.01, 0.02, 0.03] + list(np.arange(0.05, 0.55, 0.05)):
         for seed in [2356, 3487, 4942, 1432]:
             params = (
-                Parameters.from_base(mesh_size=2, seed=seed, sampling_p=sampling_p)
+                Parameters.from_base(mesh_size=2, seed=seed, sampling_p=sampling_p, use_mask=True)
                 .multi_resolution(1, p_sched=[7, 7, 7])
                 .multi_metric()
-                .gomea(fos=-6, partial_evals=True)
+                .gomea()
                 .instance(Collection.EMPIRE, 26)
-                .stopping_criteria(iterations=[200])
+                .stopping_criteria(iterations=[100])
             )
             yield Experiment(params, project)
 
 if __name__ == "__main__":
     queue = ExperimentQueue()
-    for experiment in sampling_p_range(26, "gomea_sampling"):
+    for experiment in sampling_p_range(26, "gomea_sampling_full"):
         queue.push(experiment)
