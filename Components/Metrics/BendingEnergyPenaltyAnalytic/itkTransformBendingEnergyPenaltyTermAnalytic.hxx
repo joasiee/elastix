02d25a1877fa592111ecd8ddd6fceeb6329cf238
Joas Mulder <joasmulder@hotmail.com>
2022-05-23 20:27:43 +0200

    lambda = 1

--- Components/Metrics/BendingEnergyPenaltyAnalytic/itkTransformBendingEnergyPenaltyTermAnalytic.hxx
+++ Components/Metrics/BendingEnergyPenaltyAnalytic/itkTransformBendingEnergyPenaltyTermAnalytic.hxx
@@ -32,6 +32,7 @@ template <class TFixedImage, class TScalarType>
 TransformBendingEnergyPenaltyTermAnalytic<TFixedImage, TScalarType>::TransformBendingEnergyPenaltyTermAnalytic()
 {
   this->m_RegularizationParameters.implementation = 'b';
+  this->m_RegularizationParameters.curvature_penalty = 1.0f;
 }
 
 template <class TFixedImage, class TScalarType>
