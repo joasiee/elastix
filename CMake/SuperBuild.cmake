376b806cf0aab246afc6025057bd4b2c17217058
Joas Mulder <joasmulder@hotmail.com>
2022-05-31 13:36:21 +0200

    patch command weird escaping

--- CMake/SuperBuild.cmake
+++ CMake/SuperBuild.cmake
@@ -1,8 +1,8 @@
 include(ExternalProject)
 find_package(Git REQUIRED)
 
-set (DEPENDENCIES_PREFIX "${PROJECT_SOURCE_DIR}/external")
-set (PLASTIMATCH_PATCH "${PROJECT_SOURCE_DIR}/tools/plastimatch.patch")
+set (DEPENDENCIES_PREFIX ${PROJECT_SOURCE_DIR}/external)
+set (PLASTIMATCH_PATCH ${PROJECT_SOURCE_DIR}/tools/plastimatch.patch)
 set (EXTRA_CMAKE_ARGS
     --no-warn-unused-cli
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
@@ -14,25 +14,25 @@ set (EXTRA_CMAKE_ARGS
 )
 
 
-ExternalProject_Add(
-    eigen
-    URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2
-    PREFIX ${DEPENDENCIES_PREFIX}
-    INSTALL_DIR ${DEPENDENCIES_PREFIX}/install
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:STRING=${DEPENDENCIES_PREFIX}/install ${EXTRA_CMAKE_ARGS}
-    BUILD_COMMAND ${CMAKE_COMMAND} --build ${DEPENDENCIES_PREFIX}/src/eigen-build --config Release --target all
-)
+# ExternalProject_Add(
+#     eigen
+#     URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2
+#     PREFIX ${DEPENDENCIES_PREFIX}
+#     INSTALL_DIR ${DEPENDENCIES_PREFIX}/install
+#     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:STRING=${DEPENDENCIES_PREFIX}/install ${EXTRA_CMAKE_ARGS}
+#     BUILD_COMMAND ${CMAKE_COMMAND} --build ${DEPENDENCIES_PREFIX}/src/eigen-build --config Release --target all
+# )
 
-ExternalProject_Add(
-    itk
-    GIT_REPOSITORY https://github.com/joasiee/ITK.git
-    GIT_TAG origin/gomea
-    PREFIX ${DEPENDENCIES_PREFIX}
-    INSTALL_DIR ${DEPENDENCIES_PREFIX}/install
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:STRING=${DEPENDENCIES_PREFIX}/install ${EXTRA_CMAKE_ARGS}
-    BUILD_COMMAND ${CMAKE_COMMAND} --build ${DEPENDENCIES_PREFIX}/src/itk-build --config Release --target all
-    INSTALL_COMMAND ""
-)
+# ExternalProject_Add(
+#     itk
+#     GIT_REPOSITORY https://github.com/joasiee/ITK.git
+#     GIT_TAG origin/gomea
+#     PREFIX ${DEPENDENCIES_PREFIX}
+#     INSTALL_DIR ${DEPENDENCIES_PREFIX}/install
+#     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:STRING=${DEPENDENCIES_PREFIX}/install ${EXTRA_CMAKE_ARGS}
+#     BUILD_COMMAND ${CMAKE_COMMAND} --build ${DEPENDENCIES_PREFIX}/src/itk-build --config Release --target all
+#     INSTALL_COMMAND ""
+# )
 
 
 set(PLASTIMATCH_ARGS
@@ -45,22 +45,22 @@ set(PLASTIMATCH_ARGS
 
 ExternalProject_Add(
     plastimatch
-    DEPENDS itk
+    # DEPENDS itk
     GIT_REPOSITORY https://gitlab.com/plastimatch/plastimatch.git
     GIT_TAG 3734adbfdb0b4cdce733ef1275b4e76093ffc6d5
-    PATCH_COMMAND (${GIT_EXECUTABLE} apply --check ${PLASTIMATCH_PATCH} && ${GIT_EXECUTABLE} apply ${PLASTIMATCH_PATCH}) || echo "patch applied."
+    PATCH_COMMAND ${GIT_EXECUTABLE} apply ${PLASTIMATCH_PATCH} || echo "patch already applied"
     BUILD_COMMAND ${CMAKE_COMMAND} --build ${DEPENDENCIES_PREFIX}/src/plastimatch-build --config Release --target plmregister
     PREFIX ${DEPENDENCIES_PREFIX}
     INSTALL_DIR ${DEPENDENCIES_PREFIX}/install
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:STRING=${DEPENDENCIES_PREFIX}/install ${PLASTIMATCH_ARGS} ${EXTRA_CMAKE_ARGS}
 )
 
-ExternalProject_Add (
-    elastix
-    DEPENDS eigen itk plastimatch
-    SOURCE_DIR ${PROJECT_SOURCE_DIR}
-    CMAKE_ARGS -DUSE_SUPERBUILD=OFF ${EXTRA_CMAKE_ARGS}
-    BUILD_COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_BINARY_DIR} --config ${CMAKE_BUILD_TYPE} --target all
-    INSTALL_COMMAND ""
-    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}
-)
+# ExternalProject_Add (
+#     elastix
+#     DEPENDS eigen itk plastimatch
+#     SOURCE_DIR ${PROJECT_SOURCE_DIR}
+#     CMAKE_ARGS -DUSE_SUPERBUILD=OFF ${EXTRA_CMAKE_ARGS}
+#     BUILD_COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_BINARY_DIR} --config ${CMAKE_BUILD_TYPE} --target all
+#     INSTALL_COMMAND ""
+#     BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}
+# )
