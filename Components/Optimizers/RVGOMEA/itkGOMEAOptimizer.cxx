0e058c8e0af8e2fe50f6eea7551d3625feca13d9
Joas Mulder <joasmulder@hotmail.com>
2022-06-28 17:57:27 +0200

    update default param population size

--- Components/Optimizers/RVGOMEA/itkGOMEAOptimizer.cxx
+++ Components/Optimizers/RVGOMEA/itkGOMEAOptimizer.cxx
@@ -83,7 +83,7 @@ GOMEAOptimizer::initialize(void)
   if (m_BasePopulationSize == 0.0)
   {
     if (m_MaxNumberOfPopulations == 1)
-      m_BasePopulationSize = m_PartialEvaluations ? static_cast<int>(36.1 + 7.58 * log2((double)m_NrOfParameters))
+      m_BasePopulationSize = m_PartialEvaluations ? static_cast<int>(20 + 30 * log2((double)m_NrOfParameters))
                                                   : static_cast<int>(m_NrOfParameters * 10);
     else
       m_BasePopulationSize = 10;
