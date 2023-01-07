841d1276220c8c541aace25c296c65b5f876020b
Joas Mulder <joasmulder@hotmail.com>
2023-01-07 15:58:48 +0100

    nr asgd iterations 200

--- Components/Optimizers/RVGOMEA/elxGOMEA.hxx
+++ Components/Optimizers/RVGOMEA/elxGOMEA.hxx
@@ -114,7 +114,7 @@ GOMEA<TElastix>::BeforeEachResolution(void)
   this->SetStaticLinkageMinSetSize(staticLinkageMinSetSize);
 
   /** Set NumberOfASGDIterations.*/
-  int numberOfASGDIterations = 20;
+  int numberOfASGDIterations = 200;
   this->m_Configuration->ReadParameter(
     numberOfASGDIterations, "NumberOfASGDIterations", this->GetComponentLabel(), level, 0);
   this->SetNumberOfASGDIterations(numberOfASGDIterations);
